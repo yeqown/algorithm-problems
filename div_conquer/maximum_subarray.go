e6c819012258db79d0531def26eb4878b6bfe62b
yeqown <1121583868@qq.com>
2018-02-07 21:12:28 +0800

    add tree folder and append divide conquer alg

---
+++ div_conquer/maximum_subarray.go
@@ -0,0 +1,14 @@
+// 最大子数组
+
+/*
+the maximum subarray must be:
+* left part totaly
+* right part totaly
+* cross mid position
+*/
+
+package div_conquer
+
+func MaxSubArray(nums []int) int {
+
+}
---
+++ div_conquer/maximum_subarray_test.go
@@ -0,0 +1,9 @@
+package div_conquer
+
+import (
+	"testing"
+)
+
+func Test_MaxSubArray(t *testing.T) {
+
+}
