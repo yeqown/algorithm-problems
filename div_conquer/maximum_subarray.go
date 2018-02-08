54ef3e58905dd3b9b5111437b101e1b5f7c75eeb
yeqown <1121583868@qq.com>
2018-02-08 10:48:37 +0800

    finish maximum_subarray with div_conquer solution

--- div_conquer/maximum_subarray.go
+++ div_conquer/maximum_subarray.go
@@ -2,13 +2,65 @@
 
 /*
 the maximum subarray must be:
-* left part totaly
-* right part totaly
-* cross mid position
+* left part totaly > left_max
+* right part totaly > right_max
+* cross mid position > mid_max
+* Max(left, mid, right)
 */
 
 package div_conquer
 
-func MaxSubArray(nums []int) int {
+import (
+	"math"
+)
 
+func MaxOfThree(a, b, c int) int {
+	max := a
+	if b > max {
+		max = b
+	}
+	if c > max {
+		max = c
+	}
+	return max
+}
+
+func MaxSubArray(nums []int, left, right int) int {
+	if left >= right {
+		return nums[left]
+	}
+
+	mid := (left + right) / 2
+
+	left_max := MaxSubArray(nums, left, mid)
+	right_max := MaxSubArray(nums, mid+1, right)
+	// this is so much important
+	cross_max := crossMidMaxSubArray(left, right, nums)
+
+	// this can be thinking as merge function
+	return MaxOfThree(left_max, right_max, cross_max)
+}
+
+func crossMidMaxSubArray(left, right int, nums []int) int {
+	// how to find cross mid
+	mid := (left + right) / 2
+	left_max := int(math.MinInt64)
+	right_max := int(math.MinInt64)
+
+	sumary := 0
+	for i := mid; i >= left; i-- {
+		sumary += nums[i]
+		if sumary > left_max {
+			left_max = sumary
+		}
+	}
+
+	sumary = 0
+	for i := mid + 1; i <= right; i++ {
+		sumary += nums[i]
+		if sumary > right_max {
+			right_max = sumary
+		}
+	}
+	return left_max + right_max
 }
--- div_conquer/maximum_subarray_test.go
+++ div_conquer/maximum_subarray_test.go
@@ -5,5 +5,26 @@ import (
 )
 
 func Test_MaxSubArray(t *testing.T) {
+	arr := []int{1, 2, 3, 45, 9}
+	excepted := 60
 
+	if max_sub := MaxSubArray(arr, 0, len(arr)-1); max_sub != excepted {
+		t.Fatal("want: ", excepted, "have: ", max_sub)
+	}
+}
+
+func Test_MaxSubArray_case1(t *testing.T) {
+	arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	excepted := 6
+
+	if max_sub := MaxSubArray(arr, 0, len(arr)-1); max_sub != excepted {
+		t.Fatal("want: ", excepted, "have: ", max_sub)
+	}
+}
+
+func Test_MaxOfThree(t *testing.T) {
+	var a, b, c int = 1, 2, 3
+	if max := MaxOfThree(a, b, c); max != c {
+		t.Fatal("Wrong Judge with MaxOfThree")
+	}
 }
